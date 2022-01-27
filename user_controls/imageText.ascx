d067423448f1d7d4fd0ec51ec6c66c2ec32a7b4d
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-01-27 10:24:14 +0100

    Agregado estilo a la pagina web y creado control de usuario

---
+++ user_controls/imageText.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="VB" AutoEventWireup="false" CodeFile="imageText.ascx.vb" Inherits="user_controls_WebUserControl" %>
+
+<div>
+    <asp:ImageButton ID="image" runat="server" Width="100%"/>
+    <asp:HyperLink ID="link" runat="server"></asp:HyperLink>
+</div>
---
+++ user_controls/imageText.ascx.vb
@@ -0,0 +1,34 @@
+﻿
+Partial Class user_controls_WebUserControl
+    Inherits System.Web.UI.UserControl
+
+    Private url As String
+    Private text As String
+    Private pathToImg As String
+
+    Public WriteOnly Property DestinationUrl As String
+        Set(value As String)
+            url = value
+        End Set
+    End Property
+
+    Public WriteOnly Property TextContent As String
+        Set(value As String)
+            text = value
+        End Set
+    End Property
+
+    Public WriteOnly Property ImagePath As String
+        Set(value As String)
+            pathToImg = value
+        End Set
+    End Property
+
+    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        link.NavigateUrl = "~/pages/" & url
+        link.Text = text
+        image.ImageUrl = pathToImg
+        image.PostBackUrl = "~/pages/" & url
+    End Sub
+
+End Class
